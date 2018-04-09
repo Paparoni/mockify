2502fd3c4ed88c5f6bd4b1aa38a414f114937a4e
Aj <adtune6509@student.waynecc.edu>
2018-04-09 00:58:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+function mock(text) {
+    text = text.toLowerCase();
+    var textArray = text.split('');
+    console.log(textArray);
+    for(var i = 0; i < text.length; i++){
+        var eoL = i++;
+        
+        if( typeof textArray[eoL] == "undefined"){
+            
+        } else {
+            textArray[eoL] = textArray[eoL].toUpperCase();
+        }
+        
+    }
+    
+  var final = textArray.join('');
+  return final;
+}
 
 $( document ).ready(function() {
     $("#main-box").height(200);
