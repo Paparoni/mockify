05f9c7ae8334dd5787e6a6bcc8696ee5f1a82bc3
Aj <adtune6509@student.waynecc.edu>
2018-04-09 01:09:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -20,8 +20,8 @@ function mock(text) {
 $( document ).ready(function() {
     $("#main-box").height(200);
     $("#result-box").height(200);
-    $("#main-box").width(200);
-    $("#result-box").width(200);
+    $("#main-box").width(400);
+    $("#result-box").width(400);
     
     $("#mock").click(function(){
         var text = $("#main-box").val();
